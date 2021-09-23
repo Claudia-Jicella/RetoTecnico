a79401e4636d744e943a5ce265f23875bd303052
Claudia-Jicella <cjvalenciat@gmail.com>
2021-09-23 17:12:17 -0500

    proyecto1Sofka

---
+++ src/main/java/com/example/RetoDeIngreso/RetoDeIngresoApplication.java
@@ -0,0 +1,13 @@
+package com.example.RetoDeIngreso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoDeIngresoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoDeIngresoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RetoDeIngreso/RetoDeIngresoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RetoDeIngreso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoDeIngresoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
